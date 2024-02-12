2f3ca33d35c72f00df4996264490af4c12613940
TrueDragon <ineeddonglirn@gmail.com>
2024-02-12 19:31:47 +0800

    Init commit for updated login system

--- ECOFURNITURE-main/main/User.py
+++ ECOFURNITURE-main/main/User.py
@@ -1,51 +1,33 @@
-# User class
 class User:
     count_id = 0
 
-    # initializer method
-    def __init__(self, first_name, last_name, gender, membership, remarks):
+    def __init__(self, username, password, user_type):
         User.count_id += 1
         self.__user_id = User.count_id
-        self.__first_name = first_name
-        self.__last_name = last_name
-        self.__gender = gender
-        self.__membership = membership
-        self.__remarks = remarks
+        self.__username = username
+        self.__password = password
+        self.__user_type = user_type
 
-    # accessor methods
     def get_user_id(self):
         return self.__user_id
 
-    def get_first_name(self):
-        return self.__first_name
+    def get_username(self):
+        return self.__username
 
-    def get_last_name(self):
-        return self.__last_name
+    def get_password(self):
+        return self.__password
 
-    def get_gender(self):
-        return self.__gender
+    def get_user_type(self):
+        return self.__user_type
 
-    def get_membership(self):
-        return self.__membership
-
-    def get_remarks(self):
-        return self.__remarks
-
-    # mutator methods
     def set_user_id(self, user_id):
         self.__user_id = user_id
 
-    def set_first_name(self, first_name):
-        self.__first_name = first_name
+    def set_username(self, username):
+        self.__username = username
 
-    def set_last_name(self, last_name):
-        self.__last_name = last_name
+    def set_password(self, password):
+        self.__password = password
 
-    def set_gender(self, gender):
-        self.__gender = gender
-
-    def set_membership(self, membership):
-        self.__membership = membership
-
-    def set_remarks(self, remarks):
-        self.__remarks = remarks
+    def set_user_type(self, user_type):
+        self.__user_type = user_type
